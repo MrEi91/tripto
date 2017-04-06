37d8c7b47039bac4496b33d9ac49a8761560e657
Eri Irawan <eryirawan91@gmail.com>
2017-04-06 22:10:02 +0700

    Install dependencies nativebase

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { View, Image, Text } from 'react-native'
+import { Container, Content, Tab, Tabs, TabHeading, Icon } from 'native-base';
+// import Tab1 from './tabOne';
+// import Tab2 from './tabTwo';
 
 const Header = () => {
   return (
-    <View>
-      <Text>Ini Header</Text>
-    </View>
-  )
-}
-
-const style = {
-  
+    <Container>
+    <Tabs>
+      <Tab Tab heading={ <TabHeading><Icon name="home"/><Text> Cities</Text></TabHeading> }>
+          <Text>Cities</Text>
+      </Tab>
+      <Tab heading={ <TabHeading><Icon name="person"/><Text> Profile</Text></TabHeading> }>
+          <Text>Profile</Text>
+      </Tab>
+    </Tabs>
+    </Container>
+  );
 }
 
 export default Header
