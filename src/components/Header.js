c23f5a6f68117afc9115ac8b987ee0679c5a3c5b
Eri Irawan <eryirawan91@gmail.com>
2017-04-06 17:55:16 +0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Image, Text } from 'react-native'
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Ini Header</Text>
+    </View>
+  )
+}
+
+const style = {
+  
+}
+
+export default Header
